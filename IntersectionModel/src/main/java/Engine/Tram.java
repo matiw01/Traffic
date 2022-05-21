f122896656cdba631a9f5a739196572270666e97
superiorshrimp <szymon.zychowicz300701@gmail.com>
2022-05-21 18:52:09 +0200

    tramki jezdza ladnie ale nie reaguja na swiatla

--- IntersectionModel/src/main/java/Engine/Tram.java
+++ IntersectionModel/src/main/java/Engine/Tram.java
@@ -3,6 +3,10 @@ package Engine;
 import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static java.util.Collections.*;
 
 public class Tram{
     private final int numberOfPeople;
@@ -12,13 +16,14 @@ public class Tram{
     private final TramTarget target;
     private final int length;
     private final ArrayList<TramPath> parts = new ArrayList<>();
+    private final TramPath start;
 
     public Tram(TramPath location, TramTarget target){
         this.maxVelocity = 3;
-        this.velocity = this.maxVelocity;
+        this.velocity = 1;
         this.location = location;
         this.target = target;
-
+        this.start = location;
         if(Math.random()<0.33){
             this.length = 6;
         }
@@ -33,24 +38,31 @@ public class Tram{
     }
 
     public void move(){
-        this.location = this.location.getNext(this.target);
-
+        ArrayList<TramPath> locations = new ArrayList<>();
+        for(TramPath tp : this.parts){locations.add(tp);}
+        for(int v = 0; v<this.velocity; v++){
+            this.location = this.location.getNext(this.target);
+            if(this.location != null){locations.add(this.location);}
+            else{
+                this.location = locations.get(locations.size()-1);
+                break;
+            }
+        }
         if(this.parts.size() < this.length){
-            this.parts.add(this.parts.get(this.parts.size()-1));
-        }
-        for(int i = this.parts.size()-1; i>0; i--){
-            this.parts.get(i).setOccupied(false);
-            this.parts.set(i, this.parts.get(i-1));
-        }
-        if(this.parts.size()-1 < this.length){
+            this.parts.add(locations.get(this.velocity-1));
             this.parts.get(this.parts.size()-1).setOccupied(true);
         }
-        this.parts.set(0, this.location);
-        if(this.location != null){
-            this.location.setOccupied(true);
-        }
-        else{
-            this.parts.remove(0);
+        for(int i = 0; i<this.parts.size(); i++){
+            if(i+this.velocity < locations.size()){
+                this.parts.get(i).setOccupied(false);
+                this.parts.set(i, locations.get(i+this.velocity));
+                this.parts.get(i).setOccupied(true);
+            }
+            else{
+
+                this.parts.get(i).setOccupied(false);
+                this.parts.set(i, new Rails(-1,-1)); //nie istnieje to pole - substytut nulla tutaj
+            }
         }
     }
 
